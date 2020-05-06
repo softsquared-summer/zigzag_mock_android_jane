8d9b60b5d09fb471940a13baf0c5e2e0493bb999
anyin93 <anyin93@naver.com>
2020-05-07 01:29:46 +0900

    스플래시

--- zigzag/app/src/main/java/com/example/zigzag/src/splash/SplashActivity.java
+++ zigzag/app/src/main/java/com/example/zigzag/src/splash/SplashActivity.java
@@ -2,16 +2,32 @@ package com.example.zigzag.src.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.zigzag.R;
+import com.example.zigzag.src.main.MainActivity;
 
 
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_TIME = 1500;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplication(), MainActivity.class));
+                /* 스플래시 액티비티를 스택에서 제거. */
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_TIME);
     }
+
+
 }
